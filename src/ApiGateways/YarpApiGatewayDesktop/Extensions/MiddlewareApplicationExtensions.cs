cb7cc1a8f27cade9df5357ecc473359f7c32d27e
pasternak2048 <10pka.muscatel@gmail.com>
2025-05-01 02:11:29 +0300

    [YarpApiGatewayDesktop]: updated services

--- src/ApiGateways/YarpApiGatewayDesktop/Extensions/MiddlewareApplicationExtensions.cs
+++ src/ApiGateways/YarpApiGatewayDesktop/Extensions/MiddlewareApplicationExtensions.cs
@@ -12,7 +12,8 @@
 			{
 				options.SwaggerEndpoint("https://localhost:6062/swagger/v1/swagger.json", "IdentityProvider API");
 				options.SwaggerEndpoint("https://localhost:6063/swagger/v1/swagger.json", "Catalog API");
-				options.SwaggerEndpoint("https://localhost:6069/swagger/v1/swagger.json", "Photo API");
+				options.SwaggerEndpoint("https://localhost:6069/swagger/v1/swagger.json", "Document Metadata API");
+				options.SwaggerEndpoint("https://localhost:6070/swagger/v1/swagger.json", "Document Storage API");
 				options.DocumentTitle = "API Gateway Swagger";
 				options.RoutePrefix = string.Empty;
 			});
