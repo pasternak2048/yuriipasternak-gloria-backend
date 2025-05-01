fb0f8555eaf53a838c463231b39c9f35a6dd6a6e
pasternak2048 <10pka.muscatel@gmail.com>
2025-05-01 07:32:54 +0300

    [Photo service]: removed project

--- src/Services/Photo/Photo.API/Models/DTOs/Requests/UpdateRealtyPhotoMetadataRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Photo.API.Models.DTOs.Requests
-{
-	public class UpdateRealtyPhotoMetadataRequest
-	{
-		public string? FileName { get; set; }
-
-		public string? ThumbnailUrl { get; set; }
-	}
-}
