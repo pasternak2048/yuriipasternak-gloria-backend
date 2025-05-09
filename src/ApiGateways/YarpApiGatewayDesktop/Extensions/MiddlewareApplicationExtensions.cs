232ab373d85e8ac09874f83cb0a11cafce405ccb
pasternak2048 <10pka.muscatel@gmail.com>
2025-05-09 20:32:03 +0300

    [YarpApiGatewayDesktop]: reconfigured endpoints

--- src/ApiGateways/YarpApiGatewayDesktop/Extensions/MiddlewareApplicationExtensions.cs
+++ src/ApiGateways/YarpApiGatewayDesktop/Extensions/MiddlewareApplicationExtensions.cs
@@ -10,13 +10,13 @@
 
 			app.UseSwaggerUI(options =>
 			{
-				options.SwaggerEndpoint("https://localhost:6062/swagger/v1/swagger.json", "Identity Provider API v1");
-				options.SwaggerEndpoint("https://localhost:6063/swagger/v1/swagger.json", "Catalog API v1");
-				options.SwaggerEndpoint("https://localhost:6064/swagger/v1/swagger.json", "Advert API v1");
-				options.SwaggerEndpoint("https://localhost:6069/swagger/v1/swagger.json", "Document Metadata API v1");
-				options.SwaggerEndpoint("https://localhost:6070/swagger/v1/swagger.json", "Document Storage API v1");
-				options.SwaggerEndpoint("https://localhost:6075/swagger/v1/swagger.json", "Notification API v1");
-				options.SwaggerEndpoint("https://localhost:6076/swagger/v1/swagger.json", "Subscription API v1");
+				options.SwaggerEndpoint("/identity/swagger/v1/swagger.json", "Identity Provider API v1");
+				options.SwaggerEndpoint("/catalog/swagger/v1/swagger.json", "Catalog API v1");
+				options.SwaggerEndpoint("/advert/swagger/v1/swagger.json", "Advert API v1");
+				options.SwaggerEndpoint("/documentmetadata/swagger/v1/swagger.json", "Document Metadata API v1");
+				options.SwaggerEndpoint("/documentstorage/swagger/v1/swagger.json", "Document Storage API v1");
+				options.SwaggerEndpoint("/notification/swagger/v1/swagger.json", "Notification API v1");
+				options.SwaggerEndpoint("/subscription/swagger/v1/swagger.json", "Subscription API v1");
 				options.DocumentTitle = "API Gateway Swagger";
 				options.RoutePrefix = string.Empty;
 			});
